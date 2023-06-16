28a85ff9384fd1933e28f20ef8b234b849acca62
MaheshUike43 <mah526840@gmail.com>
2023-06-16 23:09:48 +0530

    New Changes Commit

--- server/routes/petRoute.js
+++ server/routes/petRoute.js
@@ -1,25 +1,82 @@
 import express from "express";
-import petDetails from "../controllers/Pet/petDetails.js";
-import addPets from "../controllers/Pet/addPets.js";
-import allPet from "../controllers/Pet/allPet.js";
-import updatePet from "../controllers/Pet/updatePet.js";
-import deletePet from "../controllers/Pet/deletePet.js";
+import petsModel from "../models/petsModel.js";
 
 const petRoute = express.Router()
 
 //Add Pet
-petRoute.post("/pets", addPets)
+petRoute.post("/pets", async (req, res) => {
+    try {
+        const { pet_type, pet_name, breed, age, gender, photo, desc, status } = req.body;
+
+        //CREATE NEW PET
+        const newPet = { pet_type, pet_name, breed, age, gender, photo, desc, status };
+        const pet = await petsModel.create(newPet);
+
+        res.status(200).send({ success: true, message: "Pet is Added successfully", pet });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ success: false, message: "Error Adding pet" });
+    }
+})
 
 //Selected Pet Details 
-petRoute.get("/petsDetail/:id", petDetails)
+petRoute.get("/petsDetail/:id", async (req, res) => {
+    try {
+        const pet = await petsModel.findById(req.params.id)
+        if (pet) {
+            console.log(pet)
+            res.status(200).send({ success: true, message: "Pet Found", pet });
+        } else {
+            res.status(404).send({ success: false, message: "Pet Not Found" });
+        }
+    } catch (error) {
+        res.status(500).send({ success: false, message: "Internal Server Error" });
+    }
+})
 
 //All Pet Details
-petRoute.get("/allPetsDetail", allPet)
+petRoute.get("/allPetsDetail", async (req, res) => {
+    try {
+        const allpets = await petsModel.find()
+        if (allpets) {
+            res.status(200).send({ success: true, message: "Pet Found", allpets });
+        } else {
+            res.status(404).send({ success: false, message: "Pet Not Found" });
+        }
+    } catch (error) {
+        res.status(500).send({ success: false, message: "Internal Server Error" });
+    }
+})
 
 //update Pets
-petRoute.put("/pet/update/:id", updatePet)
+petRoute.put("/pet/update/:id", async (req, res) => {
+    if (req.body._id === req.params.id) {
+        try {
+            const updatePet = await petsModel.findByIdAndUpdate(req.params.id, {
+                $set: req.body,
+            });
+            console.log(updatePet)
+            res.status(200).send({ success: true, message: "Pet is updated successfully", updatePet });
+        } catch (error) {
+            res.status(500).send({ success: false, message: "Pet Not Found" });
+        }
+    } else {
+        res.status(403).send({ success: false, message: "Internal Server Error" });
+    }
+})
 
 //update Pets
-petRoute.delete("/pet/delete/:id", deletePet)
+petRoute.delete("/pet/delete/:id", async (req, res) => {
+    if (req.body._id === req.params.id) {
+        try {
+            const deletePet = await petsModel.findByIdAndDelete(req.params.id)
+            res.status(200).send({ success: true, message: "Pet is deleted successfully", deletePet });
+        } catch (error) {
+            res.status(500).send({ success: false, message: "Pet Not Found" });
+        }
+    } else {
+        res.status(403).send({ success: false, message: "Internal Server Error" });
+    }
+})
 
 export default petRoute;
